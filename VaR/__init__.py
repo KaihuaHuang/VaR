2463662f82f39081587fbd2522ef420ad1c040bf
KaihuaHuang <31960960+KaihuaHuang@users.noreply.github.com>
2018-10-20 03:37:15 -0400

    Update __init__.py

--- VaR/__init__.py
+++ VaR/__init__.py
@@ -1,2 +1,7 @@
 __author__ = "Kaihua(William) Huang (khuang41@fordham.edu)"
 __version__ = "1.0.0"
+__all__ = ['VaR','HistoricalVaR','PCAVaR']
+from .VaR import ValueAtRisk
+from .HistoricalVaR import HistoricalVaR
+from .PCAVaR import PCAVaR
+
